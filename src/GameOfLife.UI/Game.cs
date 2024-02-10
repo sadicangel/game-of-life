36d6f8eb35d2d375716627219162223c96d07d76
sadicangel <sadicangel@hotmail.com>
2024-02-10 02:28:19 +0000

    Adds seeding!

--- src/GameOfLife.UI/Game.cs
+++ src/GameOfLife.UI/Game.cs
@@ -1,55 +1,44 @@
 ﻿using Spectre.Console;
-using System.Buffers;
-using System.Runtime.InteropServices;
-using System.Text.Unicode;
 
 namespace Gol;
-public sealed class Game : IDisposable
+public sealed class Game
 {
-    private readonly unsafe byte* _grid;
-    private readonly int _size;
-    private readonly nuint _length;
+    private const byte DEAD = 0;
+    private const byte ALIVE = 1;
 
-    public Game(int size)
+    private readonly byte[] _grid;
+    private readonly byte[] _next;
+    private readonly int _size;
+
+    public Game(string seed)
     {
-        _size = size;
-        _length = checked((nuint)(size * size));
-        unsafe
+        var lines = seed.Split(default(char[]), options: StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+        if (lines.Length != lines[0].Length)
+            throw new InvalidOperationException("Only square grids supported for now");
+
+        _size = lines.Length;
+        _grid = new byte[_size * _size];
+        _next = new byte[_grid.Length];
+
+        for (int y = 0; y < lines.Length; ++y)
         {
-            _grid = (byte*)NativeMemory.AllocZeroed(_length, sizeof(byte));
-            _grid[_size + 4] = 1;
-            _grid[_size * 2 + 3] = 1;
-            _grid[_size * 2 + 4] = 1;
-            _grid[_size * 2 + 5] = 1;
+            for (int x = 0; x < lines[y].Length; ++x)
+            {
+                _grid[y * _size + x] = lines[y][x] is '*' ? ALIVE : DEAD;
+            }
         }
     }
 
     public byte this[int x, int y] { get { unsafe { return _grid[y * _size + x]; } } }
 
-    public void Greet(ReadOnlySpan<char> name)
-    {
-        Span<byte> bytes = stackalloc byte[name.Length + 1];
-        bytes[^1] = 0;
-
-        var status = Utf8.FromUtf16(name, bytes, out _, out _);
-        if (status is not OperationStatus.Done)
-            throw new InvalidOperationException($"Invalid name '{name}'");
-
-        unsafe
-        {
-            fixed (byte* ptr = bytes)
-            {
-                Library.Greet(ptr);
-            }
-        }
-    }
 
     public void Tick()
     {
         unsafe
         {
-            Span<byte> prevSpan = new Span<byte>(_grid, (int)_length);
-            Span<byte> nextSpan = stackalloc byte[(int)_length];
+            Span<byte> prevSpan = _grid;
+            Span<byte> nextSpan = _next;
             nextSpan.Clear();
 
             fixed (byte* prev = prevSpan, next = nextSpan)
@@ -58,16 +47,6 @@ public sealed class Game : IDisposable
             }
 
             nextSpan.CopyTo(prevSpan);
-        }
-    }
-
-    public unsafe void Dispose()
-    {
-        if (_grid is not null)
-        {
-            NativeMemory.Free(_grid);
-            fixed (byte** ptr = &_grid)
-                *ptr = null;
         }
     }
 
@@ -82,7 +61,7 @@ public sealed class Game : IDisposable
                 var row = new string[_size];
                 for (int x = 0; x < _size; ++x)
                 {
-                    row[x] = _grid[y * _size + x] == 1 ? "[darkorange3]*[/]" : "[silver].[/]";
+                    row[x] = _grid[y * _size + x] == ALIVE ? "[darkorange3]*[/]" : "[silver].[/]";
                 }
                 grid.AddRow(row);
             }
